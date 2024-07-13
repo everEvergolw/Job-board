8be0c0da0fd90aaf9f13ba026498a0e1690ee66f
everEvergolw <1092547508@qq.com>
2024-07-13 22:43:10 +0100

    design:creating companies

---
+++ src/app/actions/workosActions.ts
@@ -0,0 +1,21 @@
+import { WorkOS } from "@workos-inc/node";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY);
+
+
+export async function createCompany(companyName:string,userId:string){
+    'use server';
+    const org = await workos.organizations.createOrganization({name:companyName}); 
+    const om =  await workos.userManagement.createOrganizationMembership({
+        userId,
+        organizationId: org.id,
+        roleSlug: 'admin'
+
+    });
+    
+    revalidatePath('/new-listing')
+    redirect('/new-listing');
+}
